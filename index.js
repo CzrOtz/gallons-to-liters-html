bd5136451edc6a32926122b2d5a85c3aacc01d2b
CzrOtz <100041702+CzrOtz@users.noreply.github.com>
2024-06-26 13:52:25 -0400

    fixed some errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function display() {
     var gallons = document.getElementById('gallons').value;
     document.getElementById('gallonsDisplay').textContent = gallons;
     var liters = gallons * 3.78541;
-    document.getElementById('litersDisplay').textContent = liters;
+    document.getElementById('litersDisplay').textContent = liters.toFixed(2); // Fixed to two decimal places
 }
+
 
